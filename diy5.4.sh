0040dd4c12fd443bfdab136ed117704d21c7636e
zack-112 <53525365+zack-112@users.noreply.github.com>
2021-01-01 15:45:32 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -26,4 +26,5 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/ledewrt/luci-app-ledeproxy.git package/luci-app-ledeproxy
